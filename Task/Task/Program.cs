96058f68c74c4b6c29b38242e0ed05d824c51f2d
Akio532 <60925360+Akio532@users.noreply.github.com>
2021-07-13 17:51:39 +0300

    Add files via upload

---
+++ Task/Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число больше 0");
+            string num = Console.ReadLine();
+            int x = int.Parse(num);
+            Print(x);           
+        }
+
+        static void Print(int lim, int cur = 1)
+        {
+            if (lim >= cur)
+            {
+                Console.Write(cur);
+                if (cur % 3 == 0)               
+                    Console.Write(" Bar ");                                   
+                else                
+                    Console.Write(" Foo ");
+                Console.WriteLine();
+                Print(lim, cur + 1);                
+            }
+
+        }
+    }
+}
